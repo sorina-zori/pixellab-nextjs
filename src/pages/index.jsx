04003981e6f0a5b77d8dd2df091d9e81ceab3f21
sorina-am <sorina@adoreme.com>
2023-01-16 21:54:37 +0200

    escape char

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Sorina's React App</title>
+        <title>Sorina`&apos;`s React App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
